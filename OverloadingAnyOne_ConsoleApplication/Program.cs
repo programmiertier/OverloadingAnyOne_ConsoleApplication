0692cc055b74d117bfb896137e0fbfb9922ff1c9
programmiertier <zimmermannveronique@protonmail.com>
2017-11-07 13:45:30 +0100

    OverloadingAnyOne

--- OverloadingAnyOne_ConsoleApplication/Program.cs
+++ OverloadingAnyOne_ConsoleApplication/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace OverloadingAnyOne_ConsoleApplication
 {
@@ -10,6 +11,25 @@ namespace OverloadingAnyOne_ConsoleApplication
     {
         static void Main(string[] args)
         {
+            Super super = new Super();
+
+            Unter_1 u1 = new Unter_1();
+
+            Unter_2 u2 = new Unter_2();
+
+            // u1 + u1;
+            Unter_1 u1u1 = u1 + u1;
+
+            // u1 + u2;
+            Super u1u2 = u1 + u2;
+
+            // u2 + u2;
+            Unter_2 u2u2 = u2 + u2;
+
+            // u2 + u1;         // jetzt nicht das kommutativgesetz von zahlen  // möp möp
+            Super u2u1 = u2 + u1;
+
+            ReadLine();
         }
     }
 }
---
+++ OverloadingAnyOne_ConsoleApplication/Super.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace OverloadingAnyOne_ConsoleApplication
+{
+    class Super
+    {
+        // statt in Unter_1 und Unter_2 die gleichen
+        public static Super operator + (Super x, Super y)
+        {
+            WriteLine("Hier werden 2 Super Objekte addiert");
+            return new Super();
+        }
+    }
+
+    class Unter_1 : Super
+    {
+        public static Unter_1 operator + (Unter_1 x, Unter_1 y)
+        {
+            WriteLine("Hier werden 2 Unter_1 Objekte addiert");
+            return new Unter_1();
+        }
+    }
+
+    class Unter_2 : Super
+    {
+        public static Unter_2 operator + (Unter_2 x, Unter_2 y)
+        {
+            WriteLine("Hier werden 2 Unter_2 Objekte addiert");
+            return new Unter_2();
+        }
+    }
+}
